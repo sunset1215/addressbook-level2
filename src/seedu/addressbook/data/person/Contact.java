cb35233b88362b48c929d4c699d822a7a007bb31
VivianLow <vivian198912@gmail.com>
2016-09-04 15:13:38 +0800

    Extract a Contact class

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,42 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Generalization of Phone, Email and Address class
+ */
+public class Contact {
+
+	private String value;
+    private boolean isPrivate;
+    
+    public Contact() {
+    	this.value = "";
+    	this.isPrivate = false;
+    }
+    
+    public String getValue() {
+    	return this.value;
+    }
+    
+    public void setValue(String value) {
+    	this.value = value;
+    }
+    
+    public void setIsPrivate(boolean isPrivate) {
+    	this.isPrivate = isPrivate;
+    }
+    
+    @Override
+    public String toString() {
+        return value;
+    }
+    
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+    
+}
