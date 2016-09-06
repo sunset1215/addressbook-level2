cac81a05f9da95ebb393d972589f5730b0eab546
VivianLow <vivian198912@gmail.com>
2016-09-06 22:49:27 +0800

    Add unit test for IllegalValueException

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -9,10 +9,22 @@ import seedu.addressbook.data.exception.IllegalValueException;
 public class NameTest {
 
     @Test
+    public void testConstructor_throwIllegalValueExceptionIfContainNonAlphabeticCharacters() {
+        try {
+            Name name = new Name("1234!!");
+            fail("Should throw IllegalValueException");
+        } catch (IllegalValueException ive) {
+            assertEquals("Exception error message should be the same", ive.getMessage(), Name.MESSAGE_NAME_CONSTRAINTS);
+        }
+    }
+    
+    @Test
     public void testIsSimilar_differentCase() throws IllegalValueException {
         Name name = new Name("John K Smith");
         Name other = new Name("john k smith");
         assertTrue("The 2 names should be similar", name.isSimilar(other));
     }
+    
+    
     
 }
