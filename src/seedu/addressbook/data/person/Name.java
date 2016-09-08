ef176d1e409c68842c8d36bfb7758f699006daa8
VIVIAN-X250\Vivian <vivian198912@gmail.com>
2016-09-08 16:09:14 +0800

    Edit minor formatting

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -54,7 +54,7 @@ public class Name {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Name // instanceof handles nulls
-                        && this.fullName.equals(((Name) other).fullName)); // state check
+                && this.fullName.equals(((Name) other).fullName)); // state check
     }
 
     @Override
