c0ff9f914a5695f15dd9e2d20c74ddb2288717ef
VIVIAN-X250\Vivian <vivian198912@gmail.com>
2016-09-13 15:44:53 +0800

    Edit test methods name in UtilsTest.java

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -1,10 +1,14 @@
 package seedu.addressbook.common;
 
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import seedu.addressbook.common.Utils;
@@ -16,54 +20,77 @@ import seedu.addressbook.common.Utils;
  */
 public class UtilsTest {
 
-    /**
-     * Test method for {@link seedu.addressbook.common.Utils#isAnyNull(java.lang.Object[])}.
-     */
-    @Test
-    public void testIsAnyNull_noNullArgsReturnFalse() {
-        Object obj1 = new Integer(2);
-        Object obj2 = new String("String object");
-        assertFalse("Should have no null args", Utils.isAnyNull(obj1, obj2));
+    private Object nullObj;
+    private Object intObj;
+    private Object strObj;
+
+    private String unique1;
+    private String unique2;
+    private String unique3;
+
+    private String copy1;
+
+    @Before
+    public void setup() {
+        nullObj = null;
+        intObj = new Integer(2);
+        strObj = new String("I am a string");
+
+        unique1 = new String("I am unique 1");
+        unique2 = new String("I am unique 2");
+        unique3 = new String("I am unique 3");
+        copy1 = unique1;
     }
-    
+
     /**
      * Test method for {@link seedu.addressbook.common.Utils#isAnyNull(java.lang.Object[])}.
      */
     @Test
-    public void testIsAnyNull_hasNullArgsReturnTrue() {
-        Object obj1 = null;
-        Object obj2 = new String("String object");
-        assertTrue("Should have at least 1 null args", Utils.isAnyNull(obj1, obj2));
+    public void isAnyNull_noNullArgs_returnsFalse() {
+        assertFalse("Should have no null args", Utils.isAnyNull(intObj, strObj));
+    }
+
+    /**
+     * Test method for {@link seedu.addressbook.common.Utils#isAnyNull(java.lang.Object[])}.
+     */
+    @Test
+    public void isAnyNull_hasNullArgs_returnsTrue() {
+        assertTrue("Should have at least 1 null args", Utils.isAnyNull(nullObj, strObj));
     }
 
     /**
      * Test method for {@link seedu.addressbook.common.Utils#elementsAreUnique(java.util.Collection)}.
      */
     @Test
-    public void testElementsAreUnique_hasNonUniqueElementsReturnFalse() {
-        String string1 = new String("This is string 1");
-        String string2 = new String("This is string 2");
-        String string3 = string1;
+    public void elementsAreUnique_hasNonUniqueElements_returnsFalse() {
         Collection<String> collection = new ArrayList<String>();
-        collection.add(string1);
-        collection.add(string2);
-        collection.add(string3);
+        collection.add(unique1);
+        collection.add(unique2);
+        collection.add(copy1);
         assertFalse("Elements should be non-unique", Utils.elementsAreUnique(collection));
     }
-    
+
     /**
      * Test method for {@link seedu.addressbook.common.Utils#elementsAreUnique(java.util.Collection)}.
      */
     @Test
-    public void testElementsAreUnique_hasUniqueElementsReturnTrue() {
-        String string1 = new String("This is string 1");
-        String string2 = new String("This is string 2");
-        String string3 = new String("This is string 3");
+    public void elementsAreUnique_hasUniqueElements_returnsTrue() {
         Collection<String> collection = new ArrayList<String>();
-        collection.add(string1);
-        collection.add(string2);
-        collection.add(string3);
+        collection.add(unique1);
+        collection.add(unique2);
+        collection.add(unique3);
         assertTrue("Elements should be unique", Utils.elementsAreUnique(collection));
+    }
+
+    @After
+    public void teardown() {
+        intObj = null;
+        strObj = null;
+
+        unique1 = null;
+        unique2 = null;
+        unique3 = null;
+        copy1 = null;
     }
 
 }
